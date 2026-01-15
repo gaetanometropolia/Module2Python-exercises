4c19d2f0df8b1b6c6af5ac28b09ff7e1dd7aff40
gaetanometropolia <gaetano.trigili@metropolia.fi>
2026-01-15 13:40:39 +0200

    exercise 2.4

---
+++ exercise 2.4.py
@@ -0,0 +1,9 @@
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+number3 = int(input("Enter another number: "))
+sum_numbers = (number1 + number2 + number3)
+product = number1 * number2 * number3
+average = (number1 + number2 + number3) / 3
+print (sum_numbers)
+print (product)
+print (average)
