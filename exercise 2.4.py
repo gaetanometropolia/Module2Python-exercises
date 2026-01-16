892c8a35440885911ff3f49f3a2da840d57427bd
gaetanometropolia <gaetano.trigili@metropolia.fi>
2026-01-16 11:26:07 +0200

    2.4 completed and ran

--- exercise 2.4.py
+++ exercise 2.4.py
@@ -1,9 +1,18 @@
+#Write a program that asks the user for three integer numbers. The program prints out the sum, product, and average of the numbers.
 number1 = int(input("Enter a number: "))
 number2 = int(input("Enter another number: "))
 number3 = int(input("Enter another number: "))
-sum_numbers = (number1 + number2 + number3)
+
+# Calculate sum, product, and average
+sum = number1 + number2 + number3
 product = number1 * number2 * number3
-average = (number1 + number2 + number3) / 3
-print (sum_numbers)
-print (product)
-print (average)
+average = sum / 3
+
+#print results
+print(sum)
+print(product)
+print(average)
+
+
+
+
